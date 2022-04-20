120882afbb850f5fe01cdb28c33d8a7bb1db9b2a
Jordan Thomas <jthomas135@hotmail.com>
2022-04-20 13:30:22 -0700

    added event listener to set data in localStorage on saveBtn click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const currentDay = document.getElementById('currentDay')
+const currentDay = document.getElementById('currentDay');
+const saveBtn = document.getElementsByClassName('saveBtn');
 
 currentDay.textContent = moment().format('MMMM Do YYYY, h:mm a');
 
@@ -6,13 +7,20 @@ let currentHour = moment().hour();
 let hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (let i = 0; i < hour.length; i++) {
-    if(currentHour === hour[i]) {
-       document.getElementById(i).classList.add('present');
+    if (currentHour === hour[i]) {
+        document.getElementById(i).classList.add('present');
     }
-    if(currentHour < hour[i]) {
+    if (currentHour < hour[i]) {
         document.getElementById(i).classList.add('future');
     }
     else {
         document.getElementById(i).classList.add('past');
     }
-}
+    
+    saveBtn[i].addEventListener('click', function () {
+        var textArea = document.getElementById(i).value
+        var save = this.getAttribute('button-id')
+        localStorage.setItem(save, textArea);
+        console.log(localStorage)
+    });
+};
